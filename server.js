35d75d7d2cf77054f5e4060b53964f3af2099f78
sammyifelse <somarjeetthakuria@gmail.com>
2025-03-02 11:33:47 +0530

    updated url

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://clinic-frontend-ruddy.vercel.app/",
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
